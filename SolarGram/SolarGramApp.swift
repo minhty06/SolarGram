97c89ba001d15df10716e8852d7a68b242cd6109
minhty06 <106997563+minhty06@users.noreply.github.com>
2023-02-27 20:54:08 -0800

    Initial Commit

---
+++ SolarGram/SolarGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramApp.swift
+//  SolarGram
+//
+//  Created by Minh Ton on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
